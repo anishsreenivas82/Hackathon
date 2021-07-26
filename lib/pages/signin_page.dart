fdcc04dabd01371f638d2ca5f2b8fb6c76779829
anishsreenivas82 <77966360+anishsreenivas82@users.noreply.github.com>
2021-07-26 15:30:53 +0530

    Sign in

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -4,6 +4,27 @@ import 'package:flutterlogindesign/pages/signup_page.dart';
 import 'package:flutterlogindesign/widgets/Headdersignin.dart';
 import 'package:flutterlogindesign/widgets/btn_widget.dart';
 import 'package:flutterlogindesign/widgets/Headdersignup.dart';
+Route _createRoute(){
+        return PageRouteBuilder(
+          pageBuilder:(context, animation, secondaryAnimation) => RegPage(),
+          transitionDuration: Duration(milliseconds:500),
+          transitionsBuilder: (context, animation, secondaryAnimation, child){
+            const begin = Offset(1.0,0.0);
+            
+            const end = Offset.zero;
+            final tween = Tween(begin: begin,end: end);
+            const curve = Curves.easeOut;
+            final offsetAnimation = animation.drive(tween);
+          
+            final curvedAnimation = CurvedAnimation(parent: animation, curve: curve);
+            return SlideTransition(
+              
+              position: tween.animate(curvedAnimation),
+              
+              child: child,);
+          },
+           );
+      }
 
 TapGestureRecognizer _signupConditionRecognizer;
 
@@ -17,68 +38,77 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     _signupConditionRecognizer = TapGestureRecognizer()
       ..onTap = () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => RegPage()));
+        Navigator.of(context).push(_createRoute());
+
       };
+  
+
+      
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: EdgeInsets.only(bottom: 30),
-        child: Column(
-          children: <Widget>[
-            HeaderContainer2("Login"),
-            Expanded(
-              flex: 1,
+      
+      body: SingleChildScrollView(
               child: Container(
-                margin: EdgeInsets.only(left: 20, right: 20, top: 30),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    _textInput(hint: "Email", icon: Icons.person),
-                    _textInput(hint: "Password", icon: Icons.lock),
-                    Expanded(
-                      child: Center(
+              
+          padding: EdgeInsets.only(bottom: 30),
+          child: Column(
+            children: <Widget>[
+             HeaderContainer2('Login'),
+              Container(
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      _textInput(hint: "Email", icon: Icons.person),
+                      _textInput(hint: "Password", icon: Icons.lock),
+                      SizedBox(
+                        height: 20
+                      ),
+                      Center(
                         child: ButtonWidget(
                           onClick: () {
                             Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RegPage()));
+                context,
+                MaterialPageRoute(
+                    builder: (context) => RegPage()));
                           },
                           btnText: "SIGN IN",
                         ),
                       ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: 25),
-                      alignment: Alignment.center,
-                      child: Text(
-                        "Forgot Password",
-                        style: TextStyle(
-                          color: Colors.black54,
-                          decoration: TextDecoration.underline,
+                      SizedBox(
+                        height:20
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(bottom: 25),
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Forgot Password",
+                          style: TextStyle(
+                            color: Colors.black54,
+                            decoration: TextDecoration.underline,
+                          ),
                         ),
                       ),
-                    ),
-                    RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: "Don't have an account ? ",
-                            style: TextStyle(color: Colors.black38)),
-                        TextSpan(
-                            text: "Sign Up",
-                            recognizer: _signupConditionRecognizer,
-                            style: TextStyle(
-                              color: Colors.black54,
-                              decoration: TextDecoration.underline,
-                            )),
-                      ]),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
+                      RichText(
+                        
+                        text: TextSpan(children: [
+                          TextSpan(
+              text: "Don't have an account ? ",
+              style: TextStyle(color: Colors.black38)),
+                          TextSpan(
+              text: "Sign Up",
+              
+              recognizer: _signupConditionRecognizer,
+              style: TextStyle(
+                color: Colors.black54,
+                decoration: TextDecoration.underline,
+              )),
+                        ]),
+                      )
+                    ],
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
