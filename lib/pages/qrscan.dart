532d81a8f3ae1911685dd8e9c42986158694293c
anishsreenivas82 <77966360+anishsreenivas82@users.noreply.github.com>
2021-07-28 00:28:08 +0530

    1

---
+++ lib/pages/qrscan.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutterlogindesign/pages/verification.dart';
+var qrcode = '';
+
+
+
+
+Future<void> qrscan() async{
+  try {
+   qrcode = await FlutterBarcodeScanner.scanBarcode(
+    '#ff6666', 
+    'Cancel', 
+    true,
+    ScanMode.QR);
+
+    return VerifcationPage();
+    
+  }
+  on PlatformException{
+    qrcode='Error';
+  }
+
+    
+}
--- lib/pages/volunteer_page.dart
+++ lib/pages/volunteer_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterlogindesign/pages/qrscan.dart';
 
 class Volunteer extends StatefulWidget {
   @override
@@ -7,8 +8,45 @@ class Volunteer extends StatefulWidget {
 }
 
 class _VolunteerState extends State<Volunteer> {
+
+  int currentindex = 0;
+  final List children = [Text('Stats'),Column(children:[FloatingActionButton(onPressed:(){return qrscan();})]),Text('r')];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+       title: Text('My Flutter App'),
+     ),
+     body: children[currentindex], // new
+     bottomNavigationBar: BottomNavigationBar(
+       type: BottomNavigationBarType.fixed,
+       backgroundColor: Colors.blue,
+        // new
+       currentIndex: currentindex, // new
+       items: [
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.home),
+           label: ('Stats'),
+         ),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.mail),
+           label: ('QR Scan'),
+         ),
+         new BottomNavigationBarItem(
+           icon: Icon(Icons.person),
+           label: ('Previous Ver')
+         ),
+          new BottomNavigationBarItem(
+           icon: Icon(Icons.person),
+           label: ('Pending ver')
+         )
+       ],
+       onTap: (index){
+         setState(() {
+           currentindex = index;
+         });
+       },
+     ),
+    );
   }
 }
