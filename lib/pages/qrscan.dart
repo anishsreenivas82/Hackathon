865081e6356bc01654b12485393ed0b27ff3023b
anishsreenivas82 <77966360+anishsreenivas82@users.noreply.github.com>
2021-07-29 22:01:48 +0530

    hhh

--- lib/pages/qrscan.dart
+++ lib/pages/qrscan.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:flutterlogindesign/pages/verification.dart';
-var qrcode = '';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+// import 'package:flutterlogindesign/pages/verification.dart';
+// var qrcode = '';
 
 
 
 
-Future<void> qrscan() async{
-  try {
-   qrcode = await FlutterBarcodeScanner.scanBarcode(
-    '#ff6666', 
-    'Cancel', 
-    true,
-    ScanMode.QR);
+// Future<void> qrscan() async{
+//   try {
+//    qrcode = await FlutterBarcodeScanner.scanBarcode(
+//     '#ff6666', 
+//     'Cancel', 
+//     true,
+//     ScanMode.QR);
 
-    return VerifcationPage();
+//     return VerifcationPage();
     
-  }
-  on PlatformException{
-    qrcode='Error';
-  }
+//   }
+//   on PlatformException{
+//     qrcode='Error';
+//   }
 
     
-}
+// }
